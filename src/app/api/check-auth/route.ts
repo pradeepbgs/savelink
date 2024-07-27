1f85e27706d68b6a7c24aff948b66a9cc981fc62
pradeepbgs <kumarpradeepbgs@gmail.com>
2024-07-27 08:03:17 +0530

    deleted conosele.log

--- src/app/api/check-auth/route.ts
+++ src/app/api/check-auth/route.ts
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const token =  request.cookies.get('token')?.value ?? "";
-  console.log(token);
   if (!token) {
     return NextResponse.json({ isAuthenticated: false, success:false });
   }
