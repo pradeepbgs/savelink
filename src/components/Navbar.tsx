83dbc77aaa077733f69a4953971cdd0151a3065b
pradeepbgs <kumarpradeepbgs@gmail.com>
2024-07-27 20:55:50 +0530

    made navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,15 @@
 'use client'
 
 import Link from 'next/link';
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Menu, X } from 'lucide-react';
 
 const Navbar = () => {
- 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <nav className="bg-gray-800 text-white p-4">
@@ -18,8 +22,24 @@ const Navbar = () => {
             Links
           </Link>
         </div>
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-white focus:outline-none">
+            {isOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
+          </button>
+        </div>
       </div>
-     
+      {isOpen && (
+        <div className="md:hidden">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <Link href="/" className="block px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700 hover:text-blue-500 transition duration-200">
+              Home
+            </Link>
+            <Link href="/links" className="block px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700 hover:text-blue-500 transition duration-200">
+              Links
+            </Link>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
