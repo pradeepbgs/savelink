e73511d64318985adce5f46436e40d93b384911c
pradeepbgs <kumarpradeepbgs@gmail.com>
2024-08-07 18:51:26 +0530

    lets see if logout api works on production

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,20 +1,26 @@
 import { verifyToken } from "@/lib/authJWT";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
-    const token = await verifyToken(request)
-    if (!token) {
-        return NextResponse.json({ message: "Invalid token" }, { status: 401 });
+export async function GET(request:NextRequest){
+    try {
+
+        const response = NextResponse.json(
+            {
+                message:"Logged out successfully",
+                success:true
+            }
+        )
+
+        response.cookies.set('token','',{
+            httpOnly:true,
+            expires:new Date(0)
+        })
+
+        return response;
+
+    } catch (error:any) {
+
+        return NextResponse.json({error: error.message}, {status:500})
+
     }
-    
-    const response = NextResponse.json({success:true, message: "Logged out successfully" }, { status: 200 });
-    
-    response.cookies.set({
-        name: 'token', 
-        value: '',
-        expires: new Date(0),
-        path: '/',
-    });
-    
-    return response;
 }
